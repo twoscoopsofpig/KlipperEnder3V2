290eeaab14662290e92d907b6f1ee9ff7c538bd6
Bill Conant <twoscoopsofpig@gmail.com>
2024-02-26 12:48:25 -0600

    Committed 2024-02-26 12:48:25

--- klipperbackup.sh
+++ klipperbackup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+pid_gnome=$(pgrep gnome-session)
+DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/${pid_gnome}/environ|cut -d= -f2-)
+export DBUS_SESSION_BUS_ADDRESS=${DBUS_SESSION_BUS_ADDRESS}
+
 # Add all changes to the staging area
 git add .
 
