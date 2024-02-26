5118ea38ad1c4ef4479b5bc4d4eeb0edd4195ee3
Bill Conant <twoscoopsofpig@gmail.com>
2024-02-26 10:17:21 -0600

    Committed 2024-02-26 10:17:21

---
+++ klipperbackup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with a message
+commitdate="Committed $(date +%Y-%m-%d" "%H:%M:%S)"
+git commit -m "$commitdate"
+
+# Push the changes to the remote repository
+git push origin master
